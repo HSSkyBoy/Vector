8e5b83e6902ae8746628493c425678c191f278c2
Lxiaoyao077 <barkerbigh7@gmail.com>
2026-06-27 01:50:33 +0800

    Irena 9fae696: Add reset scope request action [app/src/main/java/org/lsposed/manager/ConfigManager.java]

--- app/src/main/java/org/lsposed/manager/ConfigManager.java
+++ app/src/main/java/org/lsposed/manager/ConfigManager.java
@@ -159,9 +159,19 @@ public class ConfigManager {
         }
     }
 
-    public static boolean removeBlockedScopeRequest(String packageName, int userId) {
+    public static boolean removeBlockedScopeRequest(String packageName) {
         try {
-            LSPManagerServiceHolder.getService().removeBlockedScopeRequest(packageName, userId);
+            LSPManagerServiceHolder.getService().removeBlockedScopeRequest(packageName);
+            return true;
+        } catch (RemoteException e) {
+            Log.e(App.TAG, Log.getStackTraceString(e));
+            return false;
+        }
+    }
+
+    public static boolean isVerboseLogEnabled() {
+        try {
+            return LSPManagerServiceHolder.getService().isVerboseLog();
             return true;
         } catch (RemoteException e) {
             Log.e(App.TAG, Log.getStackTraceString(e));
